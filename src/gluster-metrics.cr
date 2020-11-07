3d6b3f3e3e5311db5c4c3360fd87826a0f4ebd73
Aravinda Vishwanathapura <aravinda@kadalu.io>
2020-11-07 13:52:11 +0530

    Metrics setup
    - Two Metrics registries created, One is default Crometheus
      registry and other one is Cluster metrics registry
    - Integrated Handlers of two registries with HTTP Server.
    
    Signed-off-by: Aravinda Vishwanathapura <aravinda@kadalu.io>

--- src/gluster-metrics.cr
+++ src/gluster-metrics.cr
@@ -1,3 +1,15 @@
 require "./args"
+require "./metrics_server"
 
-puts parsed_args
+
+def main
+  args = parsed_args
+
+  # Register URL path
+  MetricsServer.default_registry.path = args.metrics_path
+  MetricsServer.cluster_metrics_registry.path = args.cluster_metrics_path
+
+  MetricsServer.start(args)
+end
+
+main
---
+++ src/metrics_server.cr
@@ -0,0 +1,31 @@
+require "http/server"
+require "http/server/handlers/compress_handler"
+require "crometheus"
+
+module MetricsServer
+  @@cluster_metrics_registry = Crometheus::Registry.new(false)
+
+  def self.default_registry
+    Crometheus.default_registry
+  end
+
+  def self.cluster_metrics_registry
+    @@cluster_metrics_registry
+  end
+
+  def self.start(args)
+    server = HTTP::Server.new([HTTP::CompressHandler.new,
+                               HTTP::LogHandler.new,
+                               HTTP::ErrorHandler.new(true),
+                               default_registry.get_handler,
+                               cluster_metrics_registry.get_handler]) do |context|
+      context.response << "Hello World"
+    end
+
+    address = server.bind_tcp "0.0.0.0", args.port
+
+    puts "Launching server at #{address}"
+    puts "Press Ctrl+C to exit"
+    server.listen
+  end
+end
