392e6f2d4787d3c653ea786973f76943ebb18906
Aravinda Vishwanathapura <aravinda@kadalu.io>
2021-08-11 10:59:34 +0530

    Metrics collection enhancements (#17)
    * Fixed XML parsing of Volume info (Fixes: #7)
    * Gluster CLI wrappers and local metrics collection moved
      to external library aravindavk/glustercli-crystal
    * Glusterd CPU and Memory metrics now uses `top` command
      instead of using `ps` command.
    * Stale metrics are now cleaned up. If a resource is deleted,
      those metrics will still show up till the exporter restarts.
      For example, deleted Volume, replaced brick or other resources.
    * [New] New API introduced for non Prometheus consumers.
      `GET /metrics.json`(Fixes: #8)
    * [Breaking] Cluster metrics and local metrics APIs are
      now combined as a single API `GET /metrics`(Fixes: #9)
    * [New] New metrics introduced:
        - Log directory size `log_directory_size_bytes` with labels
          `cluster`, `hostname` and `path`
        - Self heal daemon local metrics `shd_cpu_percentage`,
          `shd_memory_percentage` and `shd_uptime_seconds` with labels
          `cluster`, `hostname` and `volume_name`
        - Brick local metrics `brick_cpu_percentage`,
          `brick_memory_percentage` and `brick_uptime_seconds` with
          labels `cluster`, `volume_type`, `volume_state`, `volume_name`,
          `subvol_index`, `hostname`, `path`(Fixes: #4)
        - Node Uptime `node_uptime_seconds` with labels `cluster`,
          `hostname`.
    * [Breaking] Label Changes
        - Subvol labels changes:
           `type` to `volume_type`,
           `state` to `volume_state` and
           `name` to `volume_name`
        - Brick label changes:
           `type` to `volume_type`,
           `state` to `volume_state` and
           `name` to `volume_name`
    * [Breaking] Metric name changes
        - `volume_` prefix removed from Subvol metrics
        - `volume_` prefix removed from Brick metrics
    * [Breaking] Removed metrics(We can add these later if required)
        - `glusterd_resident_memory_bytes`
        - `glusterd_virtual_memory_bytes`
    * Version number automatically taken from `shards.yml` using the
      `shards version` command (Build time).
    
    Signed-off-by: Aravinda Vishwanathapura <aravinda@kadalu.io>

---
+++ src/metrics/local.cr
@@ -0,0 +1,91 @@
+module GlusterMetricsExporter
+  Crometheus.alias ShdGauge = Crometheus::Gauge[:cluster, :hostname, :volume_name]
+  Crometheus.alias LogGauge = Crometheus::Gauge[:cluster, :hostname, :path]
+
+  @@brick_cpu_percentage = BrickGauge.new(:brick_cpu_percentage, "Brick CPU Percentage")
+  @@brick_memory_percentage = BrickGauge.new(:brick_memory_percentage, "Brick Memory Percentage")
+  @@brick_uptime_seconds = BrickGauge.new(:brick_uptime_seconds, "Brick Uptime in Seconds")
+
+  @@glusterd_cpu_percentage = PeerGauge.new(:glusterd_cpu_percentage, "Glusterd CPU Percentage")
+  @@glusterd_memory_percentage = PeerGauge.new(:glusterd_memory_percentage, "Glusterd Memory Percentage")
+  @@glusterd_uptime_seconds = PeerGauge.new(:glusterd_uptime_seconds, "Glusterd Uptime in Seconds")
+  @@node_uptime_seconds = PeerGauge.new(:node_uptime_seconds, "Node Uptime in Seconds")
+  @@log_dir_size_bytes = LogGauge.new(:log_directory_size_bytes, "Log directory size in bytes")
+
+  @@shd_cpu_percentage = ShdGauge.new(:shd_cpu_percentage, "Self Heal Daemon CPU Percentage")
+  @@shd_memory_percentage = ShdGauge.new(:shd_memory_percentage, "Self Heal Daemon Memory Percentage")
+  @@shd_uptime_seconds = ShdGauge.new(:shd_uptime_seconds, "Self Heal Daemon Uptime in Seconds")
+
+  def self.clear_local_metrics
+    @@brick_cpu_percentage.clear
+    @@brick_memory_percentage.clear
+    @@brick_uptime_seconds.clear
+    @@glusterd_cpu_percentage.clear
+    @@glusterd_memory_percentage.clear
+    @@glusterd_uptime_seconds.clear
+    @@shd_memory_percentage.clear
+    @@shd_memory_percentage.clear
+    @@shd_uptime_seconds.clear
+    @@node_uptime_seconds.clear
+    @@log_dir_size_bytes.clear
+  end
+
+  handle_metrics(["local_metrics"]) do |metrics_data|
+    # Reset all Metrics to avoid stale data. Careful if
+    # counter type is used
+    clear_local_metrics
+
+    # If Local Metrics are not enabled
+    next if !@@config.enabled?("local_metrics")
+
+    metrics_data.volumes.each do |volume|
+      volume.subvols.each_with_index do |subvol, sidx|
+        subvol.bricks.each do |brick|
+          brick_labels = {
+            cluster:      @@config.cluster_name,
+            volume_name:  volume.name,
+            volume_type:  volume.type,
+            volume_state: volume.state,
+            hostname:     brick.node.hostname,
+            path:         brick.path,
+            subvol_index: "#{sidx}",
+          }
+
+          if metrics_data.local_metrics[brick.node.hostname]? && metrics_data.local_metrics[brick.node.hostname].bricks[brick.path]?
+            brick_local = metrics_data.local_metrics[brick.node.hostname].bricks[brick.path]
+
+            @@brick_cpu_percentage[**brick_labels].set(brick_local.cpu_percentage)
+            @@brick_memory_percentage[**brick_labels].set(brick_local.memory_percentage)
+            @@brick_uptime_seconds[**brick_labels].set(brick_local.uptime_seconds)
+          end
+        end
+      end
+    end
+
+    metrics_data.peers.each do |peer|
+      peer_labels = {
+        cluster:  @@config.cluster_name,
+        hostname: peer.hostname,
+      }
+
+      @@node_uptime_seconds[**peer_labels].set(metrics_data.local_metrics[peer.hostname].node_uptime_seconds)
+
+      log_labels = peer_labels.merge({path: "/var/log/glusterfs"})
+      @@log_dir_size_bytes[**log_labels].set(metrics_data.local_metrics[peer.hostname].log_dir_size_bytes)
+
+      gd_metrics = metrics_data.local_metrics[peer.hostname].glusterd
+      @@glusterd_cpu_percentage[**peer_labels].set(gd_metrics.cpu_percentage)
+      @@glusterd_memory_percentage[**peer_labels].set(gd_metrics.memory_percentage)
+      @@glusterd_uptime_seconds[**peer_labels].set(gd_metrics.uptime_seconds)
+
+      metrics_data.local_metrics[peer.hostname].shds.each do |shd|
+        # TODO: Add SHD Volname identifier when multiple SHDs exists per node
+        shd_labels = peer_labels.merge({volume_name: "all"})
+
+        @@shd_cpu_percentage[**shd_labels].set(shd.cpu_percentage)
+        @@shd_memory_percentage[**shd_labels].set(shd.memory_percentage)
+        @@shd_uptime_seconds[**shd_labels].set(shd.uptime_seconds)
+      end
+    end
+  end
+end
